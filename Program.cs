f79e23340ccdfcf4a890136ff8211d6c36f461c2
juanhernandezdeveloper <jualherac@gmail.com>
2023-01-09 14:31:52 -0600

    First commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please give me the number of words you want to write");
+int arrayNumb = Int32.Parse(Console.ReadLine());
+
+string[] arrayOfWords = new string[arrayNumb];
+
+for(int i = 0; i< arrayOfWords.Length; i++)
+{
+    Console.WriteLine($"Please write the word number {i+1}");
+    arrayOfWords[i] = Console.ReadLine();
+}
+
+Console.WriteLine("Please give me a letter to count in the list of words:");
+char wordToCount = char.Parse(Console.ReadLine());
+Console.WriteLine(wordToCount);
