cc33747bfafb84b7f412e2ab0ccb9eb2d6e7fea5
juanhernandezdeveloper <jualherac@gmail.com>
2023-01-10 22:38:48 -0600

    solution of counting repetitions

--- Program.cs
+++ Program.cs
@@ -3,12 +3,30 @@ int arrayNumb = Int32.Parse(Console.ReadLine());
 
 string[] arrayOfWords = new string[arrayNumb];
 
-for(int i = 0; i< arrayOfWords.Length; i++)
+for(int i = 0; i < arrayNumb; i++)
 {
     Console.WriteLine($"Please write the word number {i+1}");
     arrayOfWords[i] = Console.ReadLine();
 }
 
 Console.WriteLine("Please give me a letter to count in the list of words:");
-char wordToCount = char.Parse(Console.ReadLine());
-Console.WriteLine(wordToCount);
+char charToCount = Console.ReadKey().KeyChar;
+
+string newArray = "";
+int count = 0;
+
+for (int i = 0; i < arrayOfWords.Length; i++)
+{
+    newArray = string.Join("", arrayOfWords);
+}
+
+int repetitions = 0;
+
+foreach (char c in newArray)
+{
+    if (c == charToCount)
+    {
+        repetitions++;
+    }
+}
+Console.WriteLine($"\nThe leter {charToCount} is repeated {repetitions} times in the list of words");
