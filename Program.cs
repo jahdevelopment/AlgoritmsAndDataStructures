fa1e04f84d68a8106b164e9220a4c4df8e6e7783
juanhernandezdeveloper <jualherac@gmail.com>
2023-01-12 19:59:54 -0600

    Making a loop to verify the right initial prompt of number

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
-﻿Console.WriteLine("Please give me the number of words you want to write");
+﻿// prompt a user for an integer,which is the length of the array word list:
+
+Console.WriteLine("Please give me the number of words you want to write");
 int arrayNumb = Int32.Parse(Console.ReadLine());
 
+// Secure that the number has to be positive:
+
+while (arrayNumb <= 0)
+{
+    Console.WriteLine("Give a number greater than 0 (cero):");
+    arrayNumb = Int32.Parse(Console.ReadLine());
+} 
+
+//Creation of the string or arrays to store the words:
+
 string[] arrayOfWords = new string[arrayNumb];
+
+// Prompt the user for n words, one to fill each index in the array:
 
 for(int i = 0; i < arrayNumb; i++)
 {
@@ -9,8 +23,12 @@ for(int i = 0; i < arrayNumb; i++)
     arrayOfWords[i] = Console.ReadLine();
 }
 
+// Prompt the user to enter a single character to count:
+
 Console.WriteLine("Please give me a letter to count in the list of words:");
 char charToCount = Console.ReadKey().KeyChar;
+
+// Creating a new array of characters to keep all the characters given in the above string of arrays:
 
 string newArray = "";
 int count = 0;
@@ -19,6 +37,8 @@ for (int i = 0; i < arrayOfWords.Length; i++)
 {
     newArray = string.Join("", arrayOfWords);
 }
+
+// Counting the occurrencies of that character within the newArray string:
 
 int repetitions = 0;
 
@@ -29,7 +49,13 @@ foreach (char c in newArray)
         repetitions++;
     }
 }
+
+// Print the number of occurrences repeated:
+
 Console.WriteLine($"\nThe leter {charToCount} is repeated {repetitions} times in the list of words");
+
+// Calculate and print the percentage of ocurrences of that character comparing with the
+// total of the characters of the newArray string:
 
 int totalCharacters = newArray.Length;
 
