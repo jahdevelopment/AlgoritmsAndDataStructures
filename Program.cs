4b3f10f1a76918ad5e0cf6b2965e91d84905928c
juanhernandezdeveloper <jualherac@gmail.com>
2023-01-15 02:49:36 -0600

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿string initialStr = "Programmatic Python";
+
+string str = "";
+
+string repChars;
+
+
+
+for (int i = 0; i < initialStr.Length; i++)
+{
+    for (int j = i + 1; j < initialStr.Length; j++)
+    {
+        if (initialStr[i] == initialStr[j])
+        {
+            repChars = initialStr[j].ToString().ToLower();
+            Console.Write(repChars);
+        }
+    }
+}
+
+
