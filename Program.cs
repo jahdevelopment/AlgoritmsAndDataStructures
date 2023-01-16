028f0f4f951c55f6f337e9f43090eecac0b47805
juanhernandezdeveloper <jualherac@gmail.com>
2023-01-16 13:25:59 -0600

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// We have a list of integers where elements appear either once or twice.
+// Find the elements that appeared twice in O(n) time.
+// example: { 1, 2, 3, 4, 7, 9, 2, 4}
+// returns { 2, 4}
+
+int[] input = { 1, 2, 3, 4, 7, 9, 2, 4 };
+
+int[] findingPairs = new int[input.Length];
+
+string pairs = "";
+
+for(int i = 0; i < input.Length; i++)
+{
+    for(int j = 0; j < input.Length; j++)
+    {
+        if (input[i] == input[j])
+        {
+            pairs += pairs;
+        }
+    }
+}
