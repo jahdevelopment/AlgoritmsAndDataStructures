e617b7d3261e79256fcb2b8cf87d589b319f2a03
juanhernandezdeveloper <jualherac@gmail.com>
2023-01-16 14:47:15 -0600

    First commit

--- Program.cs
+++ Program.cs
@@ -5,17 +5,19 @@
 
 int[] input = { 1, 2, 3, 4, 7, 9, 2, 4 };
 
-int[] findingPairs = new int[input.Length];
-
 string pairs = "";
 
 for(int i = 0; i < input.Length; i++)
 {
-    for(int j = 0; j < input.Length; j++)
+    for(int j = i + 1; j < input.Length; j++)
     {
         if (input[i] == input[j])
         {
-            pairs += pairs;
+            pairs += input[i];
         }
     }
 }
+
+string findingPairs = string.Join(", ",pairs );
+
+Console.WriteLine(findingPairs);
