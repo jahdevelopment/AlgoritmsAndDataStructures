0e2ea9d74654d7410335934ea0a028b74e878003
juanhernandezdeveloper <jualherac@gmail.com>
2023-01-16 15:33:32 -0600

    First and second problems resolved

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿// We have a list of integers where elements appear either once or twice.
-// Find the elements that appeared twice in O(n) time.
-// example: { 1, 2, 3, 4, 7, 9, 2, 4}
-// returns { 2, 4}
+﻿// List of integers where elements appear either once or twice.
+// Find the elements that appeared twice.
+
+Console.WriteLine("******************** FIRST PROBLEM ********************\n");
 
 int[] input = { 1, 2, 3, 4, 7, 9, 2, 4 };
 
-string pairs = "";
+List<int> pairs = new List<int>();
+
 
 for(int i = 0; i < input.Length; i++)
 {
@@ -13,7 +14,7 @@ for(int i = 0; i < input.Length; i++)
     {
         if (input[i] == input[j])
         {
-            pairs += input[i];
+            pairs.Add(input[i]);
         }
     }
 }
@@ -21,3 +22,20 @@ for(int i = 0; i < input.Length; i++)
 string findingPairs = string.Join(", ",pairs );
 
 Console.WriteLine(findingPairs);
+
+Console.WriteLine("******************** SECOND PROBLEM ********************\n");
+
+// We have two sorted int arrays which could be with different sizes.
+// We need to merge them in a third array while keeping this result array sorted.
+//returns
+//{ 1, 2, 2, 3, 4, 5, 5, 7, 9, 13}
+
+int[] firstArray = new int[5] { 1, 2, 3, 4, 5 };
+int[] secondArray = new int[5] {2, 5, 7, 9, 13};
+int[] thirdArray = new int[firstArray.Length + secondArray.Length];
+
+thirdArray = firstArray.Concat(secondArray).ToArray();
+Array.Sort(thirdArray);
+
+string result = string.Join(", ", thirdArray);
+Console.WriteLine(result);
