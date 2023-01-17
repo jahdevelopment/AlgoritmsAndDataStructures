7724ae32d704f67836fcf82ae64ddb93ae6c6eb7
juanhernandezdeveloper <jualherac@gmail.com>
2023-01-16 22:41:01 -0600

    Third problem of reversing a number resolved

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
-﻿// List of integers where elements appear either once or twice.
-// Find the elements that appeared twice.
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Text;
 
 Console.WriteLine("******************** FIRST PROBLEM ********************\n");
 
+// List of integers where elements appear either once or twice.
+// Find the elements that appeared twice.
+
 int[] input = { 1, 2, 3, 4, 7, 9, 2, 4 };
 
-List<int> pairs = new List<int>();
+Console.WriteLine("Initial list:");
 
+string numberInput = string.Join(", ", input);
+
+Console.WriteLine(numberInput);
+
+List<int> pairs = new List<int>();
 
 for(int i = 0; i < input.Length; i++)
 {
@@ -21,21 +30,79 @@ for(int i = 0; i < input.Length; i++)
 
 string findingPairs = string.Join(", ",pairs );
 
+Console.WriteLine("Elements that appear twice:");
+
 Console.WriteLine(findingPairs);
 
-Console.WriteLine("******************** SECOND PROBLEM ********************\n");
+Console.WriteLine("\n******************** SECOND PROBLEM ********************\n");
 
-// We have two sorted int arrays which could be with different sizes.
-// We need to merge them in a third array while keeping this result array sorted.
-//returns
-//{ 1, 2, 2, 3, 4, 5, 5, 7, 9, 13}
+// Merging in a new int array two sorted int arrays
+// which could be with different sizes.
 
 int[] firstArray = new int[5] { 1, 2, 3, 4, 5 };
+
+string first = string.Join(", ", firstArray);
+
+Console.WriteLine($"First array:\n{first}");
+
 int[] secondArray = new int[5] {2, 5, 7, 9, 13};
+
+string second = string.Join(", ", secondArray);
+
+Console.WriteLine($"Second array:\n{second}");
+
 int[] thirdArray = new int[firstArray.Length + secondArray.Length];
 
 thirdArray = firstArray.Concat(secondArray).ToArray();
+
 Array.Sort(thirdArray);
 
-string result = string.Join(", ", thirdArray);
-Console.WriteLine(result);
+string third = string.Join(", ", thirdArray);
+
+Console.WriteLine($"Third array result:\n{third}");
+
+
+Console.WriteLine("\n******************** THIRD PROBLEM ********************\n");
+
+Console.WriteLine("Please enter a positive number composed of two or more digits: ");
+int inputNumb = Int32.Parse(Console.ReadLine());
+
+bool inputRight = true;
+
+while(inputRight)
+{
+    if (inputNumb > 0)
+    {
+        string numbToStr = inputNumb.ToString();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = numbToStr.Length - 1; i >= 0; i--)
+        {
+            sb.Append(numbToStr[i]);
+        }
+
+        string reverse = sb.ToString();
+        
+        Console.WriteLine($"Reverse number model 1 (including Zeros): {reverse}");
+        
+        int outputNumb = Int32.Parse(reverse);
+
+        Console.WriteLine($"Reverse number model 2: {outputNumb}");
+
+        inputRight = false;
+    }
+    else
+    {
+        Console.WriteLine("Please enter a positive number");
+        
+        inputNumb = Int32.Parse(Console.ReadLine());
+        
+        inputRight = true;
+    }
+}
+
+
+
+
+
