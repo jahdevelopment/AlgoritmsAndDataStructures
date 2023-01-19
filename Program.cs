42cf9806424094ef080ab635095fe141f5166e81
juanhernandezdeveloper <jualherac@gmail.com>
2023-01-18 22:03:02 -0600

    Last commit

--- Program.cs
+++ Program.cs
@@ -1,52 +1,54 @@
-﻿Console.WriteLine("---------------- FIRST PROBLEM ----------------");
+﻿using System.Numerics;
 
-// ist<int> MaxNumbersInLists(List<List<int>>) accepts as a parameter
-// a List of Lists of Integers. It returns a new list with each element
-// representing the maximum number found in the corresponding original list.
+Console.WriteLine("---------------- FIRST PROBLEM ----------------");
 
-//List<List<int>> MaxNumberInLists = new List<List<int>>();
+/*List<int> MaxNumbersInLists(List<List<int>>) accepts as a parameter
+a List of Lists of Integers. It returns a new list with each element
+ representing the maximum number found in the corresponding original list.*/
 
-//Console.WriteLine("\nHow many lists do you want to store?");
+List<List<int>> MaxNumberInLists = new List<List<int>>();
 
-//int bigList = Int32.Parse(Console.ReadLine());
+Console.WriteLine("\nHow many lists do you want to store?");
 
-//for (int i = 0; i < bigList; i++)
-//{
-//    List<int> innerList= new List<int>();
+int bigList = Int32.Parse(Console.ReadLine());
 
-//    Console.WriteLine($"\nPlease enter the number of elements of the list {i+1}");
-    
-//    int nestedList = Int32.Parse(Console.ReadLine());
+for (int i = 0; i < bigList; i++)
+{
+    List<int> innerList = new List<int>();
 
-//    for (int j = 0;j < nestedList; j++)
-//    {
-//        Console.WriteLine($"Please add the number {j+1} of the list {i+1}");
+    Console.WriteLine($"\nPlease enter the number of elements of the list {i + 1}");
 
-//        innerList.Add(Int32.Parse(Console.ReadLine()));
-//    }
-//    MaxNumberInLists.Add(innerList);
-//}
+    int nestedList = Int32.Parse(Console.ReadLine());
 
-//for (int i = 0; i < MaxNumberInLists.Count; i++)
-//{
-//    int max = 0;
-    
-//    for (int j = 0; j < MaxNumberInLists[i].Count; j++)
-//    {
-//        if (MaxNumberInLists[i][j] > max)
-//        {
-//            max = MaxNumberInLists[i][j];
-//        }
-//    }
-//Console.Write($"\nList {i + 1} has a maximum of {max}. ");
-//}
+    for (int j = 0; j < nestedList; j++)
+    {
+        Console.WriteLine($"Please add the number {j + 1} of the list {i + 1}");
+
+        innerList.Add(Int32.Parse(Console.ReadLine()));
+    }
+    MaxNumberInLists.Add(innerList);
+}
+
+for (int i = 0; i < MaxNumberInLists.Count; i++)
+{
+    int max = 0;
+
+    for (int j = 0; j < MaxNumberInLists[i].Count; j++)
+    {
+        if (MaxNumberInLists[i][j] > max)
+        {
+            max = MaxNumberInLists[i][j];
+        }
+    }
+    Console.Write($"\nList {i + 1} has a maximum of {max}. ");
+}
 
 
 Console.WriteLine("\n\n---------------- SECOND PROBLEM ----------------\n");
 
-// String HighestGrade(List<List<int>>) accepts a list of number grades among students
-// in different courses (where each list represents a single course), between 0 and 100.
-// It returns the highest grade among all students in all courses
+/*String HighestGrade(List<List<int>>) accepts a list of number grades among students
+ in different courses (where each list represents a single course), between 0 and 100.
+ It returns the highest grade among all students in all courses*/
 
 List<List<int>> HighestGrade = new List<List<int>>();
 
@@ -57,7 +59,7 @@ for (int i = 0; i < totalCourses; i++)
 {
     List<int> course = new List<int>();
 
-    Console.WriteLine($"\nPlease enter the number of students of the course {i+1}");
+    Console.WriteLine($"\nPlease enter the number of students of the course {i + 1}");
 
     int studentsNumber = Int32.Parse(Console.ReadLine());
 
@@ -74,7 +76,7 @@ int bestGrade = 0;
 
 int bestClass = 0;
 
-for (int i = 0;i < HighestGrade.Count; i++)
+for (int i = 0; i < HighestGrade.Count; i++)
 {
     for (int j = 0; j < HighestGrade[i].Count; j++)
     {
@@ -82,28 +84,94 @@ for (int i = 0;i < HighestGrade.Count; i++)
         {
             bestGrade = HighestGrade[i][j];
 
-            bestClass = j;
+            bestClass = i+1;
+            Console.WriteLine(bestClass);
         }
     }
 }
-Console.WriteLine($"\nThe highest grade is {bestGrade}, it was found in class {bestClass}.\n");
+Console.WriteLine($"\nThe highest grade is {bestGrade}, it was found in the course {bestClass}.\n");
 
 
 Console.WriteLine("\n---------------- THIRD PROBLEM ----------------\n");
 
+// List<int> OrderByLooping (List<int>) orders a list of integers, from least to greatest.
+
+List<int> unorderedList = new List<int> { 6, -2, 5, 3 };
+
 List<int> orderByLooping = new List<int>();
 
-int number = 0;
+int num1 = unorderedList[0];
+int num2 = unorderedList[1];
+int num3 = unorderedList[2];
+int num4 = unorderedList[3];
 
-bool badInput = true;
+unorderedList.ToArray();
 
-while (badInput)
+string Unordered = string.Join(", ", unorderedList);
+
+Console.WriteLine($"Unordered list:\n{Unordered}");
+
+int aux = 0;
+
+if (num1 < num2 && num1 < num3 && num1 < num4)
 {
-    Console.WriteLine("Please enter a list of numbers, pulse any letter to finish");
-    number = Int32.Parse(Console.ReadLine());
-    if (number)
-    {
+    num1 = num1;
+} 
+else if (num2 < num1 && num2 < num3 && num2 < num4)
+{
+    aux = num2;
+    num2 = num1;
+    num1 = aux;
 
-    }
 }
+else if (num3 < num1 && num3 < num2 && num3 < num4)
+{
+    aux = num3;
+    num3 = num1;
+    num1 = aux;
+}
+else if (num4 < num1 && num4 < num2 && num4 < num3)
+{
+    aux = num4;
+    num4 = num1;
+    num1 = aux;
+}
+
+if (num2 < num3 && num2 < num4)
+{
+    num2 = num2;
+}
+else if (num3 < num2 && num3 < num4)
+{
+    aux = num3;
+    num3 = num2;
+    num2 = aux;
+}
+else if (num4 < num2 && num4 < num3)
+{
+    aux = num4;
+    num4 = num2;
+    num2 = aux;
+}
+
+if (num3 > num4)
+{
+    aux = num4;
+    num4 = num3;
+    num3 = aux;
+}
+
+orderByLooping.Add(num1);
+orderByLooping.Add(num2);
+orderByLooping.Add(num3);
+orderByLooping.Add(num4);
+
+orderByLooping.ToArray();
+
+string ordered = string.Join(", ", orderByLooping);
+
+Console.WriteLine($"\nOrdered List;\n{ordered}");
+
+
+
 
